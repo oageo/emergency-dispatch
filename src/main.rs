b745637729ff7dc6c8d46e6ef453346f6d8c19ff
oageo <59683387+oageo@users.noreply.github.com>
2025-04-07 07:12:12 +0900

    main.rsで札幌市の取得を毎回行わないようにコメントアウト

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use emergency_dispatch::parse::parse_011002::return_011002;
 fn main() {
     println!("This software is emergency_dispatch. Developed by oageo.");
     println!("This software is released under the Apache 2.0 license and source code is available at https://github.com/oageo/emergency-dispatch.");
-    return_011002();
+    // return_011002();
 }
