5bd4c29e8252c167b8e1299b81bb3c6b3394a05b
oageo <59683387+oageo@users.noreply.github.com>
2025-09-11 04:52:51 +0900

    江別市を追加

--- src/lib.rs
+++ src/lib.rs
@@ -125,8 +125,9 @@ pub fn to_half_width(s: &str) -> String {
 }
 
 use crate::parse::parse_011002::return_011002;
-use crate::parse::parse_012343::return_012343;
 use crate::parse::parse_012025::return_012025;
+use crate::parse::parse_012173::return_012173;
+use crate::parse::parse_012343::return_012343;
 use crate::parse::parse_022098::return_022098;
 use crate::parse::parse_062103::return_062103;
 use crate::parse::parse_083020::return_083020;
@@ -147,6 +148,7 @@ use crate::parse::parse_401307::return_401307;
 pub fn get_all() -> Result<(), Box<dyn std::error::Error>> {
     return_011002()?; 
     return_012025()?;
+    return_012173()?;
     return_012343()?;
     return_022098()?;
     return_062103()?;
