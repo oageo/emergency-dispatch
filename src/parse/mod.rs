a20450742f96da9fad24138f9297f20f2554cd6d
oageo <59683387+oageo@users.noreply.github.com>
2025-11-09 01:23:05 +0900

    mod.rsの編集漏れを修正

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -18,6 +18,7 @@ pub mod parse_122025;
 pub mod parse_122173;
 pub mod parse_122190;
 pub mod parse_122297;
+pub mod parse_122360;
 pub mod parse_123471;
 pub mod parse_123498;
 pub mod parse_141003;
